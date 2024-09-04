a3923d72b15f086d3302d0c08a10f562ae65128e
shadrach-tayo <shadrachtemitayo@gmail.com>
2024-09-04 12:54:02 -0500

    clean up

--- src/app/Provider.tsx
+++ src/app/Provider.tsx
@@ -6,7 +6,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
-import { PropsWithChildren, useState } from "react";
+import { PropsWithChildren } from "react";
 
 function makeQueryClient() {
   return new QueryClient({
@@ -37,18 +37,6 @@ function getQueryClient() {
 }
 
 export default function Provider({ children }: PropsWithChildren<{}>) {
-  // const [queryClient] = useState(
-  //   () =>
-  //     new QueryClient({
-  //       defaultOptions: {
-  //         queries: {
-  //           // With SSR, we usually want to set some default staleTime
-  //           // above 0 to avoid refetching immediately on the client
-  //           staleTime: 60 * 1000,
-  //         },
-  //       },
-  //     })
-  // );
   const queryClient = getQueryClient();
   return (
     <ThemeProvider
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,6 @@ export const metadata: Metadata = {
   description: "",
 };
 
-// const queryClient = new QueryClient();
-
 export default function RootLayout({
   children,
 }: Readonly<{
