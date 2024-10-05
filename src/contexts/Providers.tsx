1916a9b26fb91cc1d638dabb465fba174e28d931
shadrach <shadrachtemitayo@gmail.com>
2024-10-04 19:01:10 -0500

    add more pages for community management

--- src/contexts/Providers.tsx
+++ src/contexts/Providers.tsx
@@ -1,11 +1,17 @@
 import { PropsWithChildren } from "react";
 import PanelProvider from "./PanelProvider";
 import ThemeContext from "@/app/ThemeProvider";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { getQueryClient } from "@/lib/get-query-client";
 
 export default function Providers(props: PropsWithChildren<unknown>) {
+  const queryClient = getQueryClient()
+
   return (
     <ThemeContext>
-      <PanelProvider>{props.children}</PanelProvider>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <PanelProvider>{props.children}</PanelProvider>
+      </HydrationBoundary>
     </ThemeContext>
   );
 }
