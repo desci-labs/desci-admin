6785e394c3566232357501afd3692307096a0fa0
shadrach <shadrachtemitayo@gmail.com>
2024-10-04 20:45:44 -0500

    refactor communityForm and reuse for add/edit community page

--- src/app/actions.tsx
+++ src/app/actions.tsx
@@ -110,3 +110,36 @@ export async function createCommunity(_prevState: any, formData: FormData) {
     return { ok: false, message: "Unknown error occurred", error: [] };
   }
 }
+
+export async function updateCommunity(_prevState: any, formData: FormData) {
+  const id = formData.get('communityId');
+  formData.delete('communityId');
+
+  if (!id) {
+    return { ok: false, message: 'No community ID', error: []}
+  }
+  const res = await fetch(`${API_URL}/v1/admin/communities/${id}`, {
+    method: "PUT",
+    body: formData,
+    headers: { 'cookie': cookies().toString()},
+    credentials: "include",
+  });
+  let response = (await res.json()) as
+    | { data: { community: any } }
+    | { message: string; error?: string[] };
+
+  if (res.ok && "data" in response) {
+    // Set cookie
+    return {
+      ok: true,
+    };
+  } else if (!("data" in response)) {
+    return {
+      ok: false,
+      message: response.message,
+      error: response.error,
+    };
+  } else {
+    return { ok: false, message: "Unknown error occurred", error: [] };
+  }
+}
